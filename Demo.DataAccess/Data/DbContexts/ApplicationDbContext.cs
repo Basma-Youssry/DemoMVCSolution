edd526e1b2cb8ab6dd4eaf6de22d92be302b37e6
7777777 <7777777@DESKTOP-FHSN7EE>
2025-03-23 17:35:32 +0200

    03.Department Module[Migrations] and DbContext

---
+++ Demo.DataAccess/Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DataAccess.Data.Configurations;
+
+namespace Demo.DataAccess.Data.DbContexts
+{
+    internal class ApplicationDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("ConnectionString");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //For Less Configurations
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+
+            //For more Configurations
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly);
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
---
+++ Demo.DataAccess/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using Demo.DataAccess.Modules;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
