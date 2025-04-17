1f449353ad75d739ed8fa176db37eba37fc45816
7777777 <7777777@DESKTOP-FHSN7EE>
2025-04-17 16:29:10 +0200

    07.AuthLayout

---
+++ Demo.presentation/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Register
+        
+        public IActionResult Register() => View();
+       //Login
+       //Logout
+    }
+}
